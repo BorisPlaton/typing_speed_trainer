82a36d4720726832018f31a01d022048e9fc7e1c
Kirill <pnchkirill@gmail.com>
2022-06-21 06:41:16 +0300

    Typing trainer templates

--- typing_speed_trainer/config/urls.py
+++ typing_speed_trainer/config/urls.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 
@@ -7,6 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('trainer/', include('trainer.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
